26f9df5fdaeb08c7bff469c4b8c781e8b79d61fd
刘景照 <1649541226@qq.com>
2021-09-20 08:04:32 +0800

    feat:  add 包含 和 前序遍历

--- Tree/src/NewBST.java
+++ Tree/src/NewBST.java
@@ -13,7 +13,7 @@ public class NewBST<E extends Comparable<E>> {
   private Node root;
   private int size;
 
-  public BST() {
+  public NewBST() {
     root = null;
     size = 0;
   }
@@ -44,4 +44,49 @@ public class NewBST<E extends Comparable<E>> {
     return node;
 
   }
+
+  public boolean contains(E e) {
+    return contains(root, e);
+  }
+
+  private boolean contains(Node node, E e) {
+    if (node == null) {
+      return false;
+    }
+
+    if (e.compareTo(node.e) == 0) {
+      return true;
+    } else if (e.compareTo(node.e) < 0) {
+      return contains(node.left, e);
+    } else {
+      return contains(node.right, e);
+    }
+
+  }
+
+  public void preOrder() {
+    preOrder(root);
+  }
+
+  private void preOrder(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    System.out.println(node.e);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+
+  public static void main(String[] args) {
+    NewBST<Integer> bst = new NewBST<>();
+    int[] items = { 3, 1, 5, 2, 6, 7, 4 };
+    for (int i = 0; i < items.length; ++i) {
+      bst.add(items[i]);
+    }
+
+    bst.preOrder();
+
+  }
+
 }
