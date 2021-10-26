fc3da957b81f1a1227f38a83fa556630b14a7142
刘景照 <1649541226@qq.com>
2021-10-26 10:15:15 +0800

    feat: mybatis

---
+++ spring-boot-mybatis/src/main/java/com/example/springbootmybatis/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.springbootmybatis;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Martin
+ */
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许证书（cookies）
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+}
