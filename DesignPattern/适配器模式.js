e486df2b25faa702a2cebc09d4fc6cb839a4d422
刘景照 <1649541226@qq.com>
2021-12-21 15:17:42 +0800

    feat：适配器

--- DesignPattern/适配器模式.js
+++ DesignPattern/适配器模式.js
@@ -2,27 +2,26 @@
 // 微信 getUserProfile
 // 抖音 getUserInfo
 
-
 var wx = {
-  getUserProfile: function() {
+  getUserProfile: function () {
     console.log('获取微信用户信息');
-  }
-};
-
-var tt = {
-  getUserInfo: function() {
-    console.log('获取抖音用户信息');
-  }
-};
-
-var wxHandel = {
-  getUserInfo: function(){
-    return wx.getUserProfile();
   },
 };
 
+var tt = {
+  getUserInfo: function () {
+    console.log('获取抖音用户信息');
+  },
+};
 
-function getUserInfo(target){
+var wxHandel = {
+  // getUserInfo: function(){
+  //   return wx.getUserProfile();
+  // },
+  getUserInfo: wx.getUserProfile,
+};
+
+function getUserInfo(target) {
   target.getUserInfo();
   // 获取到用户信息进行别的操作
 }
