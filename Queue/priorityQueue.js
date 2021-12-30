8c841669de1d24bd799f76a5586ee8e345f536ec
刘景照 <1649541226@qq.com>
2021-12-30 16:14:53 +0800

    PriorityQueue

---
+++ Queue/priorityQueue.js
@@ -0,0 +1,44 @@
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element, priority) {
+    const queueElement = { element, priority };
+    if (this.isEmpty) {
+      this.items.push(queueElement);
+    } else {
+      const preIndex = this.items.findIndex(item => queueElement.priority < item.priority);
+      if (preIndex > -1) {
+        this.items.splice(preIndex, 0, queueElement);
+      } else {
+        this.items.push(queueElement);
+      }
+    }
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  front() {
+    return this.items[0];
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  get size() {
+    return this.items.length;
+  }
+
+  get isEmpty() {
+    return !this.items.length;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
