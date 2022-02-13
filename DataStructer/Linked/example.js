9474b8f45ee050d8ad19c8d343ee6bc996fd9d9b
刘景照 <1649541226@qq.com>
2022-02-13 22:00:01 +0800

    链表核心

---
+++ DataStructer/Linked/example.js
@@ -0,0 +1,17 @@
+// 翻转链表 
+let prev = "head";
+while(curr) {
+  next = curr.next;
+  curr.next = prev;
+  prev = curr;
+  curr = next;
+}
+
+
+
+// 查找链表中间结点  快慢指针
+
+while(fast?.next) {
+  fast = fast.next.next;
+  slow = slow.next;
+}
