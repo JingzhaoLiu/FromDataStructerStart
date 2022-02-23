2becdc99753ccc53de96f31a9dc28561ccca06d7
刘景照 <1649541226@qq.com>
2022-02-23 11:54:40 +0800

    array

---
+++ DataStructer/Array/arr.js
@@ -0,0 +1,21 @@
+var arr1 = new Array(); // 创建一个空数组
+var arr2 = new Array(3); // 创建一个3个空位的数组
+var arr3 = new Array('3'); // 创建一个第一位是字符串3的数组
+
+// 一位数字就是创建 多少大小的空数组
+// 不是数字或者多位就是创建 有内容的数组
+
+
+// push pop unshift shift
+
+// splice 删除 替换任意位置元素 
+// 索引可以是负的 
+// 返回 删除的元素 或者空数组 
+// 原数组改变
+
+// slice(begin,end)
+// 浅拷贝包括 
+// begin，不包括 end 支持负值
+
+
+
