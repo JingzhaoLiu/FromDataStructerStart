4d353b4ab4001dd48e44b5ea429225a9a1af41a6
刘景照 <1649541226@qq.com>
2022-02-23 08:58:38 +0800

    数组交集

--- DataStructer/Map/map.js
+++ DataStructer/Map/map.js
@@ -1,1 +1,18 @@
 // 字典
+
+//349. 数组交集 
+var intersection = function(nums1, nums2) {
+    const map = new Map();
+    nums1.forEach(e=>{
+        map.set(e,true)
+    })
+
+    const result = [];
+    nums2.forEach(e=>{
+        if(map.get(e)){
+            result.push(e)
+            map.delete()
+        }
+    })
+    return result
+};
