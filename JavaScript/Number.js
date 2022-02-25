1960df7dbc5f494d665addaa80c9e4ee419f135e
刘景照 <1649541226@qq.com>
2022-02-25 14:35:39 +0800

    js

---
+++ JavaScript/Number.js
@@ -0,0 +1,20 @@
+parseInt("3.5"+ "5.1") // 3
+0 / 0 // NaN
+4 / 0 // Infinity
+-5/ 0 // -Infinity
+Boolean('false') // true
+
+
+
+// 自动截取第一个非数字的字符之后的字符
+parseInt("3.14是圆周率") // 3
+parseInt("314是圆周率") // 314
+parseInt("31.5") // 31
+parseInt("-31.5") // -31
+parseInt("圆周率是3.14") // NaN
+
+parseFloat("3.14是圆周率") // 3.14
+parseFloat("314是圆周率") // 314
+parseFloat("31.5") // 31.5
+parseFloat("-31.5") // -31.5
+parseFloat("圆周率是3.14") // NaN // ?
