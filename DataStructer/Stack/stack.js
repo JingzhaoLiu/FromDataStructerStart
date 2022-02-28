4903965c60e9c57d436156b215d31a14825fb8ce
刘景照 <1649541226@qq.com>
2022-02-28 18:00:47 +0800

    stack

--- DataStructer/Stack/stack.js
+++ DataStructer/Stack/stack.js
@@ -6,6 +6,10 @@ stack.push(2);
 const item1 = stack.pop();
 const item2 = stack.pop();
 
+
+
+
+
 /*****栈的应用场景******/
 
 // 十进制转二进制：后出来的余数反而排到前面，余数依次入栈，依次出栈，实现余数倒序输出
@@ -109,21 +113,40 @@ func1();
 class Stack {
   
   constructor() {
-    this.stack = [];
+    this.items = [];
   }
 
   push(e) {
-    this.stack.push(e);
+    this.items.push(e);
   }
 
   pop() {
-    return this.stack.pop();
+    return this.items.pop();
   }
 
   isEmpty() {
-    return this.stack.length === 0;
+    return this.items.length === 0;
+  }
+
+  peek(){
+    return this.items[this.items.length - 1];
+  }
+
+  size(){
+    return this.items.length;
+  }
+
+  clear(){
+    this.items = [];
   }
 }
+
+// push(element(s)):添加一个(或几个)新元素到栈顶。
+// pop():移除栈顶的元素，同时返回被移除的元素。
+// peek():返回栈顶的元素，不对栈做任何修改(该方法不会移除栈顶的元素，仅仅返回它)。
+// isEmpty():如果栈里没有任何元素就返回 true，否则返回 false。
+// clear():移除栈里的所有元素。 this.items = [];
+// size():返回栈里的元素个数。该方法和数组的 length 属性很类似。
 
 
 //输入需要转化的十进制数
